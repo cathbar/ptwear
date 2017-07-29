d42a9d4d2325afa631a1a9f827517bd3e391f626
root <root@ip-172-31-3-108.ec2.internal>
2017-07-29 05:39:15 +0000

    final demo

--- post_test.sh
+++ post_test.sh
@@ -3,4 +3,9 @@
 #curl -H "Content-Type: application/json" -X POST -d "{"exercise_name":"Gut Buster", "sensor_data": [{"time:": 0.0, "x": 0.0, "y": 0.0, "z": 0.0},{"time:": 0.0, "x": 10.0, "y": 0.0, "z": 0.0},{"time:": 0.0, "x": 20.0, "y": 0.0, "z": 0.0}]}" http://localhost:5000/ptwear/api/v1.0/reps 
 #curl -H "Content-Type: application/json" -X POST -d "{"exercise_name": "shoulder buster", "sensor_data": [{"y": 0, "x": 0, "z": 0, "time": 0}, {"y": 205, "x": 115, "z": 300, "time": 1}, {"y": 20, "x": 15, "z": 115, "time": 10}]}" http://localhost:5000/ptwear/api/v1.0/reps 
 #curl -H "Content-Type: application/json" -X POST -d "{"shotType": "Type 1", "time_id": 1500959134185, "sensor_data": {"rot_z": 0.041813991963863373, "rot_y": 0.0096010398119688034, "rot_x": -0.088323228061199188, "acc_z": -0.11028144508600235, "acc_y": -0.61829781532287598, "acc_x": 0.66272741556167603}}" http://localhost:5000/ptwear/api/v1.0/reps
-curl -H "Content-Type: application/json" -X POST -d '{"shotType": "Type 1", "time_id": 1500959134185, "sensor_data": {"rot_z": 0.041813991963863373, "rot_y": 0.0096010398119688034, "rot_x": -0.088323228061199188, "acc_z": -0.11028144508600235, "acc_y": -0.61829781532287598, "acc_x": 0.66272741556167603}}' http://localhost:5000/ptwear/api/v1.0/reps
+
+
+#curl -H "Content-Type: application/json" -X POST -d '{"shotType": "Type 1", "time_id": 1500959134185, "sensor_data": {"rot_z": 0.041813991963863373, "rot_y": 0.0096010398119688034, "rot_x": -0.088323228061199188, "acc_z": -0.11028144508600235, "acc_y": -0.61829781532287598, "acc_x": 0.66272741556167603}}' http://localhost:5000/ptwear/api/v1.0/reps
+#curl -H "Content-Type: application/json" -X POST -d '{"shotType": "Type -1", "time_id": 1500959134185, "sensor_data": {"rot_z": 0.041813991963863373, "rot_y": 0.0096010398119688034, "rot_x": -0.088323228061199188, "acc_z": -0.11028144508600235, "acc_y": -0.61829781532287598, "acc_x": 0.66272741556167603}}' http://localhost:5000/ptwear/api/v1.0/reps
+curl -H "Content-Type: application/json" -X POST -d '{"score": 12}' http://localhost:5000/ptwear/api/v1.0/score
+
--- rest_server.py
+++ rest_server.py
@@ -1,12 +1,25 @@
-#!flask/bin/python
-from flask import Flask, jsonify, abort, request, make_response, url_for
+#!/usr/bin/python2.7
+###flask/bin/python
+from flask import Flask, jsonify, abort, request, make_response, url_for, send_file
 import os.path
 import json,random
+import findspark
+findspark.init(spark_home='/data/spark15')
+from pyspark import SparkContext
+sc = SparkContext()
 from pyspark.mllib.tree import RandomForest, RandomForestModel
 from pyspark.mllib.util import MLUtils
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.plotly as py
+from plotly.graph_objs import *
+import plotly
 
 app = Flask(__name__, static_url_path = "")
-last_rep = {}
+base_dir = 'file:///root/ptwear/'
+
 @app.errorhandler(400)
 def not_found(error):
     return make_response(jsonify( { 'error': 'Bad request' } ), 400)
@@ -14,55 +27,6 @@ def not_found(error):
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
-
-reps = [
-    {
-    'exercise_name': 'butt turdler',
-    'sensor_data': [
-        {
-            'time': 0.0,
-            'x': 0.0,
-            'y': 0.0,
-            'z': 0.0,
-        },
-        {
-            'time': 1.0,
-            'x': 10.0,
-            'y': 0.0,
-            'z': 0.0,
-        },
-        {
-            'time': 2.0,
-            'x': 20.0,
-            'y': 0.0,
-            'z': 0.0,
-        }
-        ]
-    },
-    {
-    'exercise_name': 'shoulder buster',
-    'sensor_data': [
-        {
-            'time': 0.0,
-            'x': 0.0,
-            'y': 0.0,
-            'z': 0.0,
-        },
-        {
-            'time': 1.0,
-            'x': 0.0,
-            'y': 10.0,
-            'z': 0.0,
-        },
-        {
-            'time': 2.0,
-            'x': 0.0,
-            'y': 20.0,
-            'z': 0.0,
-        }
-        ]
-    },
-]
 
 def make_public_rep(rep):
     new_rep = {}
@@ -75,8 +39,7 @@ def make_public_rep(rep):
 
 @app.route('/ptwear/api/v1.0/last_rep', methods = ['GET'])
 def get_last_rep():
-    #return last_rep
-    last_rep = {"exercise_name": "This shouldn't happen"}
+    last_rep = {"ERROR": "Couldn't find a last rep. This shouldn't happen."}
     if os.path.isfile('last_rep.txt'): 
         with open('last_rep.txt', 'r') as the_file:
             last_rep=the_file.read().replace('\n', '')
@@ -84,6 +47,7 @@ def get_last_rep():
 
 @app.route('/ptwear/api/v1.0/reps', methods = ['POST'])
 def create_rep():
+    print "Got a post request!"
     if not request.get_json():
         abort(400)
     shot_obj = request.get_json()
@@ -91,9 +55,8 @@ def create_rep():
     print shot_obj
 
     cls = int(shot_obj['shotType'].replace("Type ", ""))
-    shot_obj[shotType] = cls
+    shot_obj['shotType'] = cls
     id = shot_obj['time_id']
-    hit = random.random() < shot_probs[cls]
     sensor_data = shot_obj['sensor_data']
     acc_x = sensor_data['acc_x'] 
     acc_y = sensor_data['acc_y'] 
@@ -102,41 +65,141 @@ def create_rep():
     rot_y = sensor_data['rot_y'] 
     rot_z = sensor_data['rot_z'] 
     shot_obj['shotType'] = cls
-    last_rep['hit'] = hit
     if cls == -1:
         #Test shot
+        print "Received test shot"
+        data_test = MLUtils.loadLibSVMFile(sc, base_dir + "test_shot.txt")
+        predictions = model.predict(data_test.map(lambda x: x.features)).collect()
+        prediction = int(predictions[0])
+        hit = random.random() < shot_probs[prediction]
+        shot_obj['hit'] = int(hit)
+        print "Predicting: {0}".format(prediction)
+        shot_obj['shotType'] = int(prediction)
         with open('last_rep.txt', 'w') as the_file:
-            the_file.write("{0}\n".format(last_rep))
+            the_file.write("{0}\n".format(shot_obj))
         with open('test_shot.txt', 'w') as the_file:
             the_file.write("{0}  1:{1} 2:{2} 3:{3} 4:{4} 5:{5} 6:{6}\n".format(cls, acc_x, acc_y, acc_z, rot_x, rot_y, rot_z))
-        data_test = MLUtils.loadLibSVMFile(sc, "test_shot.txt")
-        predictions = model.predict(testData.map(lambda x: x.features))
-        print str(predictions.collect())
+        with open('predictions.txt', 'a') as pred_file:
+            for pred in predictions:
+                pred_file.write("{0}\n".format(int(pred)))
+        print "Predicting: {0}, {1}".format(prediction, hit)
+        return str((prediction, hit))
     else:
         #Training shot
-        with open('all_reps.txt', 'a') as the_file:
+        print "Received training shot"
+        #with open('all_reps.txt', 'a') as the_file:
+        with open('training_data.txt', 'a') as the_file:
             the_file.write("{0}  1:{1} 2:{2} 3:{3} 4:{4} 5:{5} 6:{6}\n".format(cls, acc_x, acc_y, acc_z, rot_x, rot_y, rot_z))
-
-
-    
-
-
     print "CLS: " + str(cls)
-    return jsonify( { 'rep': make_public_rep(last_rep) } ), 201
+    return jsonify( { 'rep': make_public_rep(shot_obj) } ), 201
+
+@app.route('/ptwear/api/v1.0/score', methods = ['POST'])
+def create_score():
+    print "Got a score post request!"
+    if not request.get_json():
+        abort(400)
+    score_obj = request.get_json()
+    print str(score_obj)
+    with open('scores.txt', 'a') as score_file:
+        score_file.write("{0}\n".format(int(score_obj['score'])))
+    return get_score()
+
+
+@app.route('/ptwear/api/v1.0/score', methods = ['GET'])
+def get_score():
+    with open('scores.txt') as f:
+        scores = f.readlines()
+    if len(scores) != 0:
+        scores = [int(x.strip()) for x in scores] 
+    
+    return str(sorted(scores, reverse=True)[:10])
+
+@app.route('/ptwear/api/v1.0/get_image')
+def get_image():
+    filename = 'predictions_distribution.png'
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    shot_types = ('Normal', 'Chest Hurl', 'Shotput', 'Granny', 'Overhead', 'Overhand')
+    y_pos = np.arange(len(shot_types))
+    
+    with open('predictions.txt') as f:
+        content = f.readlines()
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    if len(content) != 0:
+        content = [int(x.strip()) for x in content] 
+    
+    shot_counts = [content.count(0), content.count(1), content.count(2), content.count(3), content.count(4), content.count(5)]
+    ax.barh(y_pos, shot_counts, align='center', color='green')
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(shot_types)
+    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_xlabel('Count')
+    ax.set_title('Prediction Distribution')
+    fig.savefig(filename)
+
+    data = Data([ Histogram(x=content) ])
+    plot_url = py.plot(data, filename='Predictions')
+
+    return send_file(filename, mimetype='image/png')
+
+@app.route('/ptwear/api/v1.0/get_image2')
+def get_image2():
+    filename = 'predictions_over_time.png'
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    #y_pos = np.arange(len(shot_types))
+    
+    shot_probs = [.9, .6, .2, .5, .2, .05]
+    with open('predictions.txt') as f:
+        content = f.readlines()
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    if len(content) != 0:
+        content = [int(x.strip()) for x in content] 
+    xs = range(1, len(content)+1)
+    shot_counts = content
+    scores = []
+    for shot in shot_counts:
+        offset = random.random()%.25
+        if random.random() < .5:
+            offset = -offset
+        new_score = shot*shot_probs[shot] + offset
+        if new_score < 0:
+            new_score = 0
+        elif new_score > 1:
+            new_score = 1
+        scores.append(new_score)
+    ax.plot(xs, scores, '-.', color='green')
+    ax.plot(xs, scores, '*', ms=15, color='blue')
+    ax.set_xticks(xs)
+    ax.set_xticklabels(xs)
+    ax.set_xlabel('Shot #')
+    ax.set_title('Performance Over Time')
+    fig.savefig(filename)
+
+
+
+    data = Data([ Scatter(x=xs, y=scores) ])
+    plot_url = py.plot(data, filename='PerformanceOverTime')
+
+    return send_file(filename, mimetype='image/png')
 
 def train_model(train_filename):
     # Load and parse the data file into an RDD of LabeledPoint.
-    trainingData= MLUtils.loadLibSVMFile(sc, train_filename)
+    trainingData= MLUtils.loadLibSVMFile(sc, base_dir + train_filename)
     # Train a RandomForest model.
     #  Empty categoricalFeaturesInfo indicates all features are continuous.
     #  Note: Use larger numTrees in practice.
     #  Setting featureSubsetStrategy="auto" lets the algorithm choose.
-    model = RandomForest.trainClassifier(trainingData, numClasses=3, categoricalFeaturesInfo={},
+#    model = RandomForest.trainClassifier(trainingData, numClasses=6, categoricalFeaturesInfo={},
+#                                         numTrees=3, featureSubsetStrategy="auto",
+#                                         impurity='gini', maxDepth=4, maxBins=32)
+    model = RandomForest.trainClassifier(trainingData, numClasses=4, categoricalFeaturesInfo={},
                                          numTrees=3, featureSubsetStrategy="auto",
                                          impurity='gini', maxDepth=4, maxBins=32)
     return model
 
 if __name__ == '__main__':
-    last_rep = reps[0]
+    plotly.tools.set_credentials_file(username='dkenny', api_key='I3maDPBABGUpd6My6ad2')
+    print sc.parallelize([1,2,3]*1000).count()
     model = train_model('training_data.txt')
     app.run(debug = True, host='0.0.0.0')
